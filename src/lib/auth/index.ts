2950efb2b2dece1b8dbf96764dc6d3daf3c4c92d
yash101 <lodhad@gmail.com>
2025-04-21 10:47:53 -0700

    Made API easier to use

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,11 +1,4 @@
-import * as AppConfig from './appconfig';
-import * as AuthTokens from './tokens';
-import * as Auth from './auth';
-import * as IDToken from './idtoken';
-
-export {
-  AppConfig,
-  AuthTokens,
-  Auth,
-  IDToken,
-};
+export * from './appconfig';
+export * from './tokens';
+export * from './auth';
+export * from './idtoken';
